02684793e705b69dbd62f76e0a8a593726f304ec
LucasMouratoCH <132353766+LucasMChave@users.noreply.github.com>
2025-05-18 16:48:21 -0300

    Update index.jsx

--- FEnd/src/pages/Home/index.jsx
+++ FEnd/src/pages/Home/index.jsx
@@ -1,21 +1,25 @@
-import '../style.css'
-import MedLogo from '../../assets/MedOrg.svg'
-
-function Home() {
-  return (
-      <div className='telaLogin'>
-        <form>
-          <img src={MedLogo}/>
-          <h1>Login</h1>
-          <input email='' type='email'/>
-          <input senha='' type='text'/>
-          <button id='create' type='button'>Criar Conta</button>
-          <button id='enter' type='button'>Entrar</button>
-          
-        </form>
-      </div>
-      
-  )
-}
-
-export default Home
+return (
+    <div className="telaLogin">
+      <form id="login">
+        <img src={MedLogo} alt="MedOrg Logo" />
+        <h1>Login</h1>
+        
+        <input 
+          type="email" 
+          name="email" 
+          placeholder="Digite seu email" 
+          required 
+        />
+        
+        <input 
+          type="password" 
+          name="senha" 
+          placeholder="Digite sua senha" 
+          required 
+        />
+        
+        <button id="create" type="button">Criar Conta</button>
+        <button id="enter" type="submit">Entrar</button>
+      </form>
+    </div>
+  );
