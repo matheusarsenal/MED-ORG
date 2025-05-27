e640ed411e5b93c62c5b92075ce97ce7db0a027b
PheonixEdson <135137092+PheonixEdson@users.noreply.github.com>
2025-05-26 23:58:57 -0300

    Update deletar.php

--- BackEnd/src/paciente/deletar.php
+++ BackEnd/src/paciente/deletar.php
@@ -5,21 +5,16 @@ header("Content-Type: application/json; charset=UTF-8");
 
 include(__DIR__ . '/../conexao.php');
 
-$dados = json_decode(file_get_contents("php://input"), true);
 
-if (!$dados || !isset($dados['id'])) {
-    echo json_encode(["mensagem" => "ID não informado."]);
-    exit;
-}
+$data = json_decode(file_get_contents('php://input'), true);
+$id = $data['id'];
 
-$id = mysqli_real_escape_string($conn, $dados['id']);
-
-$sql = "DELETE FROM agendamento WHERE id='$id'";
+$sql = "DELETE FROM paciente WHERE id=$id";
 
 if ($conn->query($sql) === TRUE) {
-    echo json_encode(["mensagem" => "Agendamento deletado com sucesso."]);
+    echo json_encode(["mensagem" => "Paciente deletado com sucesso."]);
 } else {
-    echo json_encode(["mensagem" => "Erro ao deletar: " . $conn->error]);
+    echo json_encode(["erro" => "Erro: " . $conn->error]);
 }
 
 $conn->close();
